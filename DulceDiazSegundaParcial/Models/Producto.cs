54178b0237998504eb47f3f0342dd13d17c9ceae
Colibecas <Colibecas@HP255G8>
2024-10-21 19:16:45 -0600

    Agregar archivos de proyecto.

---
+++ DulceDiazSegundaParcial/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DulceDiazSegundaParcial.Model
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El número de inventario es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser mayor a cero.")]
+
+        public int Numero { get; set; }
+        [Required(ErrorMessage = "El nombre no debe ser vacio")]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar departamento")]
+        public string Departamento { get; set; }
+    }
+}
